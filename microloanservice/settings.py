cde76409c40100a76b98f7dc1a135f7979af85cc
RaihanDewonEman <63227528+RaihanDewonEman@users.noreply.github.com>
2021-09-23 22:38:46 +0600

    Update settings.py

--- microloanservice/settings.py
+++ microloanservice/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x8@(n4@+wsu^#j08sdt_=3cm1f0v6$tjdt70w!inkt2j8%(^#m'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
